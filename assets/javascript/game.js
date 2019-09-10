749aa08fb4216d2b5fded85e3a993b4af04595ff
Kyle Barnhouse <skyline6965@gmail.com>
2019-09-09 22:59:35 -0500

    added underscores, need to work on wins counter

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,60 +1,47 @@
 // create an array
-var wordList = ["thundercat", "ruturama", "riddick"];
+var wordList = ["quick", "fast", "track"];
 // choose word randomly
 var randNum = Math.floor(Math.random() * wordList.length);
 var choosenWord = wordList[randNum];
-console.log(choosenWord);
-console.log(randNum);
-var underScore = [];
-console.log(choosenWord);
+var correctWord = [];
+var wrongWord = [];
+var underScore = []; 
 
+var docUnderScore = document.getElementsByClassName("answers"); 
+// ^^^^ used getElementbyID, and was unsuccessful. Changed the HTML and the JS to "Class Name" and worked. Whats the difference??
+
+
+// console.log(randNum);
+
+console.log(choosenWord);
+// create underscores based on length of word
 var generateUnderScore = () => {
     for (var i = 0; i < choosenWord.length; i++) {
-        underScore.push('_');
+        underScore.push('_');        
     }
     return underScore;
 }
-console.log(generateUnderScore());
 
 
+// get users guess
 
+document.addEventListener('keypress', (event) => {
+    var keycode = event.keyCode;
+    var keyword = String.fromCharCode(keycode);
+    // console.log(keyword);
+    if(choosenWord.indexOf(keyword) > -1) {
+        correctWord.push(keyword);
+        
+        underScore[choosenWord.indexOf(keyword)] = keyword;
+        docUnderScore[0].innerHTML = underScore.join(' ');
+            if(underScore.join('') == choosenWord) {
+                alert("You Win");
+            }
+    }
+    else {wrongWord.push(keyword);
+        
+    }
+});
 
-// var wins = [];
-// var userGuess = [];
-// var correctGuess = [];
-// var remainingGuesses = [];
-
-// console.log(wordList);
-// console.log(wins);
-// console.log(userGuess);
-// console.log(correctGuess);
-// console.log(remainingGuesses);
-
-// document.onkeyup = function() {
-//     var userGuess = String.fromCharCode(event.keyCode).toLowerCase();
-    
-
-
-// function runGame(arr) {
-         
-//     for (var i = 0; i < wordList.length; i++){
-//         if (userGuess === wordList[i]) {
-//             console.log("Congrats ");            
-//         }
-//         else {
-//             console.log("Guess again ");
-//         }
-//     }
-// }
-// }
-
-
-
-
-
-
-
-// onkeyup event that checks the wordList.
-// create array of words
-// 
+docUnderScore[0].innerHTML = generateUnderScore().join(' ');
 
